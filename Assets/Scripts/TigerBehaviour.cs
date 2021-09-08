ad26b9069eed9ddc5953364206980701ff27c8fd
Ivan1Kot <wocker@i.ua>
2021-09-08 19:42:15 +0300

    Добавил тигра

---
+++ Assets/Scripts/TigerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class TigerBehaviour : MonoBehaviour
+{
+    [SerializeField] private string _animationName;
+    [SerializeField] private GameObject _tiger;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "MainCamera")
+        {
+            _tiger.SetActive(true);
+            _animator.Play(_animationName);
+        }
+    }
+}
